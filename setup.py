d52fd3710e3868e49a2e6b1c62fc960f08267ac8
Kevin Shen <kshen@braid2.cnsi.ucsb.edu>
2022-07-21 01:06:02 -0700

    setup.py for basic install

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name = 'idptools',
+      version = '0.1dev',
+      description = 'idp tools',
+      packages = ['idptools'],
+      author = 'Kevin Shen',
+      author_email = 'kevin.shen@ucsb.edu'
+     )
