6ffe6a4b8aedb0ad1ed10b23f3f24704816bc0d9
Kevin Shen <kshen@braid2.cnsi.ucsb.edu>
2022-07-22 00:14:43 -0700

    added initial implicit solvent script; also provide command line entry point

--- idptools/cmdln.py
+++ idptools/cmdln.py
@@ -3,7 +3,8 @@
 # requires each module to expose their own customized handling of commandline arguments
 # another potential paradigm is to have each module have their own dispatchers.
 #
-import sys, argparse
+import os, sys, argparse
+import utility.log as log
 from . import config
 from . import aa
 from . import tleap
@@ -15,5 +16,87 @@ def dispatch():
 
     if args.cmd == "pyleap":
         tleap.cmdln(unknown)
+    elif args.cmd == "implicit_relax":
+        implicit_relax(unknown)
+    elif args.cmd == "ELP_homo":
+        ELP_homo(unknown)
     else:
         print("Unrecognized command: {}".format(args.cmd))
+
+# ===== functions =====
+# -----> create homo-ELP sequence
+#pyleap -q VPGVGVPGVGVPGVGVPGVGVPGVG -ff ff14SBonlysc -r -n V5
+def ELP_homo(cmdln_args):
+    parser = argparse.ArgumentParser("Make ELP chain and relax using ff14SBonlysc + igb8 (defaults)")
+    parser.add_argument("X",type=str,help="Guest residue")
+    parser.add_argument("n",type=int,help="number of pentamer repeats")
+    parser.add_argument("-centered",action="store_true",help="use centered repeat definition PGXGV")
+    parser.add_argument("-reversed",action="store_true",help="use reversed repeat definition XGPVG following Chilkoti")
+    args = parser.parse_args(cmdln_args)
+    X,nrepeat = args.X,args.n
+
+    if args.centered:
+        pentamer = "PG{}GV".format(X)
+        name = f"{X}{nrepeat}_centered"
+    elif args.reversed:
+        pentamer = "{}GPVG".format(X)
+        name = f"{X}{nrepeat}_reversed"
+    else:
+        pentamer = "VPG{}G".format(X)
+        name = f"{X}{nrepeat}"
+    seq = pentamer*nrepeat
+
+    cmd = f"pyleap -q {seq} -ff ff14SBonlysc -w igb8 -n {name} -r"
+    print("executing: {}".format(cmd))
+    os.system(cmd)
+
+
+# -----> simple implicit solvent relaxation on whatever system is in settings.json
+#python ~/lib/idp/idptools/scripts/md_implicit.py test -ps 1000 -stride 100 -solv 5 > z.out &
+#implicit_relax(ps=None,stride=None,T=None,salt=None,**kwargs):
+def implicit_relax(cmdln_args=None):
+    parser = argparse.ArgumentParser("simple implicit solvent relaxation")
+    parser.add_argument("-ps",type=int,default=None,help="ps")
+    parser.add_argument("-stride",type=int,default=None,help="stride (ps)")
+    parser.add_argument("-T",type=float,default=None,help="T (K)")
+    parser.add_argument("-salt",type=float,default=None,help="salt (M)")
+    parser.add_argument("-device",type=int,default=0,help="GPU device")
+    if cmdln_args is None:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(cmdln_args)
+    ps,stride,T,salt = args.ps,args.stride,args.T,args.salt
+
+    settings = log.json_load("settings.json")
+    if "implicit_relax" not in settings:
+        implicit_relax = {"ps":1000*500, "stride":100, "T":298.15, "salt":0.1}
+    else:
+        implicit_relax = settings["implicit_relax"]
+
+    if ps is not None:
+        implicit_relax["ps"] = ps
+    if stride is not None:
+        implicit_relax["stride"] = stride
+    if T is not None:
+        implicit_relax["T"] = T
+    if salt is not None:
+        implicit_relax["salt"] = salt
+    
+    cmd = "python " + config.path + "/scripts/md_implicit.py"
+    cmd += " {}".format(settings["name"])
+    if settings["water"].startswith("igb"):
+       cmd += " -solv {}".format( int(settings["water"][3]) )
+    cmd += " -ps {}".format(implicit_relax["ps"])
+    cmd += " -stride {}".format(implicit_relax["stride"])
+    cmd += " -T {}".format(implicit_relax["T"])
+    cmd += " -salt {}".format(implicit_relax["salt"])
+    cmd += " -device {}".format(args.device)
+
+    print("executing: {}".format(cmd))
+    settings["implicit_relax"] = implicit_relax
+    log.json_write("settings.json",settings)
+
+    os.system(cmd)
+
+
+
---
+++ idptools/scripts/md_implicit.py
@@ -0,0 +1,91 @@
+# simple MD script
+from __future__ import division, print_function
+
+import sys
+
+# OpenMM Imports
+import openmm as mm
+import openmm.app as app
+
+# ParmEd Imports
+from parmed import load_file, unit as u
+from parmed.openmm import StateDataReporter, NetCDFReporter
+import mdtraj
+
+# Other imports
+import argparse
+parser = argparse.ArgumentParser("run implicit solvent")
+parser.add_argument("prefix",type=str,help="prefix for .parm7 and .rst7 files")
+parser.add_argument("-ps",default=1000,type=int,help="number of picoseconds to run for")
+parser.add_argument("-solv",default=8,type=int,choices=[0,1,2,5,7,8],help="igb version")
+parser.add_argument("-T",default=298.15,type=float,help="Temp (K)")
+parser.add_argument("-salt",default=0.1,type=float,help="salt conc (M)")
+parser.add_argument("-stride",default=100,type=int,help="stride for trajectory output (ps)")
+parser.add_argument("-init",default=None,help="pdb file for initial coordinates")
+parser.add_argument("-device",default=0,type=int,help="device")
+args = parser.parse_args()
+
+impSolvDict = {0:None, 1:app.HCT, 2:app.OBC1, 5:app.OBC2, 7:app.GBn, 8:app.GBn2}
+impSolv = impSolvDict[args.solv]
+dt = 2.0 #fs
+steps_per_ps = int(1000/dt)
+stride = args.stride * steps_per_ps
+
+# Load the Amber files
+print('Loading AMBER files...')
+amber_def = load_file(f'{args.prefix}.parm7', f'{args.prefix}.rst7')
+
+
+# Create the OpenMM system
+print('Creating OpenMM System')
+system = amber_def.createSystem(nonbondedMethod=app.NoCutoff,
+                               constraints=app.HBonds, implicitSolvent=impSolv,
+                               implicitSolventSaltConc=args.salt*u.moles/u.liter,
+)
+
+# Create the integrator to do Langevin dynamics
+integrator = mm.LangevinIntegrator(
+                        args.T*u.kelvin,       # Temperature of heat bath
+                        1.0/u.picoseconds,  # Friction coefficient
+                        dt*u.femtoseconds, # Time step
+)
+
+# Define the platform to use; CUDA, OpenCL, CPU, or Reference. Or do not specify
+# the platform to use the default (fastest) platform
+platform = mm.Platform.getPlatformByName('CUDA')
+prop = dict(CudaPrecision='mixed',CudaDeviceIndex=str(args.device)) # Use mixed single/double precision
+
+# Create the Simulation object
+sim = app.Simulation(amber_def.topology, system, integrator, platform, prop)
+
+# Set the particle positions
+if args.init is None:
+    sim.context.setPositions(amber_def.positions) #CHANGE TO USE PDB AND MDTRAJ
+else:
+    #struc = mdtraj.load(args.init) #for now, should be a pdb file
+    pdb = app.PDBFile(args.init) 
+    sim.context.setPositions(pdb.positions)
+
+# Minimize the energy
+print('Minimizing energy')
+sim.minimizeEnergy(maxIterations=500)
+
+# Set up the reporters to report energies and coordinates every 100 steps
+sim.reporters.append(
+        StateDataReporter(sys.stdout, steps_per_ps, step=True, potentialEnergy=True,
+                               kineticEnergy=True, temperature=True)
+)
+sim.reporters.append(
+        #NetCDFReporter(f'{args.prefix}.nc', 1000, crds=True) # CHANGE TO USE NETCDF
+        mdtraj.reporters.DCDReporter(f'{args.prefix}.dcd', stride)
+)
+
+# Run dynamics
+positions = sim.context.getState(getPositions=True).getPositions()
+app.PDBFile.writeFile(sim.topology, positions, open("init.pdb", 'w'))
+
+print('Running dynamics')
+sim.step(steps_per_ps*args.ps)
+
+positions = sim.context.getState(getPositions=True).getPositions()
+app.PDBFile.writeFile(sim.topology, positions, open("final.pdb", 'w'))
